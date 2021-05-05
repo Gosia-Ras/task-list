253d080151867d27174cdfeb9d4c62ba90770dc8
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-05-05 17:29:41 +0200

    adding new task and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@
     const render = () => {
         let htmlString = "";
 
-        for(const task of tasks) {
+        for (const task of tasks) {
             htmlString += `
-            <li>
+            <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
             ${task.content}
             </li>
             `;
@@ -24,9 +24,33 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTask) => {
+        tasks.push({
+            content: newTask,
+
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTask = document.querySelector(".js-newTask").value.trim();
+
+        if (newTask === "") {
+            return;
+        }
+
+        addNewTask(newTask);
+    };
+
     const init = () => {
         render();
 
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
